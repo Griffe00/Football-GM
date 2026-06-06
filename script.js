b4c37d12411e82a7d893106a0ab96e80604dc53e
Griffe00 <pristinebas@gmail.com>
2026-06-06 10:58:08 +1000

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function simulateMatch() {
-  let team1 = Math.floor(Math.random() * 5);
-  let team2 = Math.floor(Math.random() * 5);
-
-  document.getElementById("result").innerText =
-    "Team A " + team1 + " - " + team2 + " Team B";
-}
